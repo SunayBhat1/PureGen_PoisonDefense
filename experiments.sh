0bf4b36572b2c8ef6bde95a088c2c32b3c42b240
Sunay Bhat <sunaybhat1@ucla.edu>
2024-04-21 05:23:32 +0000

    minor

--- experiments.sh
+++ experiments.sh
@@ -25,18 +25,18 @@
 # Compare Diffusion eps 8
 (
 # python3 train_classifier.py --remote_user 'sunaybhat';
-python3 train_classifier.py --remote_user 'sunaybhat' --data_key 'EBM[cinic10_imagenet_ep120_nf32]_Steps[500]_T[0.0001]';
-python3 train_classifier.py --remote_user 'sunaybhat' --data_key 'EBM[cinic10_imagenet_ep120_nf32]_Steps[500]_T[0.0001]_DM_UNET_SMALL[cinic10_imagenet_nf64_modeX0_mcmc100_ep80]_Steps[[0, 1, 1]]';
-python3 train_classifier.py --remote_user 'sunaybhat' --data_key 'EBM[cinic10_imagenet_ep120_nf32]_Steps[500]_T[0.0001]_DM_UNET_SMALL[cinic10_imagenet_nf64_modeEps_mcmc100_ep90]_Steps[[0, 1, 1]]';
-python3 train_classifier.py --remote_user 'sunaybhat' --data_key 'EBM[cinic10_imagenet_ep120_nf32]_Steps[500]_T[0.0001]_DM_UNET_SMALL[cinic10_imagenet_EBMGuided_F10_nf64_modeX0_mcmc100_ep80]_Steps[[0, 1, 1]]';
-python3 train_classifier.py --remote_user 'sunaybhat' --data_key 'EBM[cinic10_imagenet_ep120_nf32]_Steps[500]_T[0.0001]_DM_UNET_SMALL[cinic10_imagenet_EBMGuided_F10_nf64_modeEps_mcmc100_ep80]_Steps[[0, 1, 1]]';
+python3 train_classifier.py --remote_user 'sunaybhat' --data_key 'EBM[cinic10_imagenet_ep120_nf32]_Steps[1000]_T[0.0001]';
+python3 train_classifier.py --remote_user 'sunaybhat' --data_key 'EBM[cinic10_imagenet_ep120_nf32]_Steps[1000]_T[0.0001]_DM_UNET_SMALL[cinic10_imagenet_nf64_modeX0_mcmc100_ep80]_Steps[[0, 1, 1]]';
+python3 train_classifier.py --remote_user 'sunaybhat' --data_key 'EBM[cinic10_imagenet_ep120_nf32]_Steps[1000]_T[0.0001]_DM_UNET_SMALL[cinic10_imagenet_nf64_modeEps_mcmc100_ep90]_Steps[[0, 1, 1]]';
+python3 train_classifier.py --remote_user 'sunaybhat' --data_key 'EBM[cinic10_imagenet_ep120_nf32]_Steps[1000]_T[0.0001]_DM_UNET_SMALL[cinic10_imagenet_EBMGuided_F10_nf64_modeX0_mcmc100_ep80]_Steps[[0, 1, 1]]';
+python3 train_classifier.py --remote_user 'sunaybhat' --data_key 'EBM[cinic10_imagenet_ep120_nf32]_Steps[1000]_T[0.0001]_DM_UNET_SMALL[cinic10_imagenet_EBMGuided_F10_nf64_modeEps_mcmc100_ep80]_Steps[[0, 1, 1]]';
 # Compare Diffusion eps 16
 # python3 train_classifier.py --remote_user 'sunaybhat' --noise_eps_narcissus 16;
-python3 train_classifier.py --remote_user 'sunaybhat' --noise_eps_narcissus 16 --data_key 'EBM[cinic10_imagenet_ep120_nf32]_Steps[500]_T[0.0001]';
-python3 train_classifier.py --remote_user 'sunaybhat' --noise_eps_narcissus 16 --data_key 'EBM[cinic10_imagenet_ep120_nf32]_Steps[500]_T[0.0001]_DM_UNET_SMALL[cinic10_imagenet_nf64_modeX0_mcmc100_ep80]_Steps[[0, 1, 1]]';
-python3 train_classifier.py --remote_user 'sunaybhat' --noise_eps_narcissus 16 --data_key 'EBM[cinic10_imagenet_ep120_nf32]_Steps[500]_T[0.0001]_DM_UNET_SMALL[cinic10_imagenet_nf64_modeEps_mcmc100_ep90]_Steps[[0, 1, 1]]';
-python3 train_classifier.py --remote_user 'sunaybhat' --noise_eps_narcissus 16 --data_key 'EBM[cinic10_imagenet_ep120_nf32]_Steps[500]_T[0.0001]_DM_UNET_SMALL[cinic10_imagenet_EBMGuided_F10_nf64_modeX0_mcmc100_ep80]_Steps[[0, 1, 1]]';
-python3 train_classifier.py --remote_user 'sunaybhat' --noise_eps_narcissus 16 --data_key 'EBM[cinic10_imagenet_ep120_nf32]_Steps[500]_T[0.0001]_DM_UNET_SMALL[cinic10_imagenet_EBMGuided_F10_nf64_modeEps_mcmc100_ep80]_Steps[[0, 1, 1]]';
+python3 train_classifier.py --remote_user 'sunaybhat' --noise_eps_narcissus 16 --data_key 'EBM[cinic10_imagenet_ep120_nf32]_Steps[1000]_T[0.0001]';
+python3 train_classifier.py --remote_user 'sunaybhat' --noise_eps_narcissus 16 --data_key 'EBM[cinic10_imagenet_ep120_nf32]_Steps[1000]_T[0.0001]_DM_UNET_SMALL[cinic10_imagenet_nf64_modeX0_mcmc100_ep80]_Steps[[0, 1, 1]]';
+python3 train_classifier.py --remote_user 'sunaybhat' --noise_eps_narcissus 16 --data_key 'EBM[cinic10_imagenet_ep120_nf32]_Steps[1000]_T[0.0001]_DM_UNET_SMALL[cinic10_imagenet_nf64_modeEps_mcmc100_ep90]_Steps[[0, 1, 1]]';
+python3 train_classifier.py --remote_user 'sunaybhat' --noise_eps_narcissus 16 --data_key 'EBM[cinic10_imagenet_ep120_nf32]_Steps[1000]_T[0.0001]_DM_UNET_SMALL[cinic10_imagenet_EBMGuided_F10_nf64_modeX0_mcmc100_ep80]_Steps[[0, 1, 1]]';
+python3 train_classifier.py --remote_user 'sunaybhat' --noise_eps_narcissus 16 --data_key 'EBM[cinic10_imagenet_ep120_nf32]_Steps[1000]_T[0.0001]_DM_UNET_SMALL[cinic10_imagenet_EBMGuided_F10_nf64_modeEps_mcmc100_ep80]_Steps[[0, 1, 1]]';
 )
 
 
@@ -76,7 +76,7 @@ scp /Users/sunaybhat/Documents/GitHub/Research/data/CIFAR10_TRAIN_Split.pth suna
 (
 # Clone 
 mkdir data;
-git clone https://github.com/SunayBhat1/PureGen_Defense
+git clone https://github.com/SunayBhat1/PureGen_PoisonDefense
 # Create a data dir
 pip install tqdm;
 pip install pandas;
