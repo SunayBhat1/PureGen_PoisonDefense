a1a8ace42ff20e9ce30ab632f3c0658aa1702662
Sunay Bhat <56206141+SunayBhat1@users.noreply.github.com>
2024-05-02 22:13:44 -0700

    minor

--- experiments.sh
+++ experiments.sh
@@ -2,62 +2,26 @@
 # Nodes Lists #
 ###############
 
-# POOD Narcissus
-
-### Node1: 
-
-# Purify Narc Eps 16
-python3 purify.py --remote_user 'sunaybhat' --diff_model None --ebm_model 'EBMSNGAN32' --ebm_name 'cifar10_nf[128]' --ebm_nf 128 --num_proc 8 --ebm_lang_steps 2000,1000,750;
-python3 purify.py --remote_user 'sunaybhat' --diff_model None --ebm_name 'cinic10_imagenet_nf[32]' --num_proc 8 --ebm_lang_steps 2000,1000,750;
-python3 purify.py --remote_user 'sunaybhat' --diff_model None --ebm_name 'office_home_nf[32]' --num_proc 8 --ebm_lang_steps 2000,1000,750;
-python3 purify.py --remote_user 'sunaybhat' --diff_model None --ebm_name 'flowers102_nf[32]' --num_proc 8 --ebm_lang_steps 2000,1000,750;
-python3 purify.py --remote_user 'sunaybhat' --diff_model None --ebm_name 'lfw_people_nf[32]' --num_proc 8 --ebm_lang_steps 2000,1000,750;
-python3 purify.py --remote_user 'sunaybhat' --diff_model None --ebm_name 'textures_nf[32]' --num_proc 8 --ebm_lang_steps 2000,1000,750;
-
-# Train Classifier
-for i in 2000 1000 750
-do
-    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "EBMSNGAN32[cifar10_nf[128]]_Steps[$i]_T[0.0001]" --poison_type 'Narcissus' --noise_eps_narcissus 16;
-    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "EBM[cinic10_imagenet_nf[32]]_Steps[$i]_T[0.0001]" --poison_type 'Narcissus' --noise_eps_narcissus 16;
-    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "EBM[office_home_nf[32]]_Steps[$i]_T[0.0001]" --poison_type 'Narcissus' --noise_eps_narcissus 16;
-    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "EBM[flowers102_nf[32]]_Steps[$i]_T[0.0001]" --poison_type 'Narcissus' --noise_eps_narcissus 16;
-    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "EBM[lfw_people_nf[32]]_Steps[$i]_T[0.0001]" --poison_type 'Narcissus' --noise_eps_narcissus 16;
-    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "EBM[textures_nf[32]]_Steps[$i]_T[0.0001]" --poison_type 'Narcissus' --noise_eps_narcissus 16;
-done
-
-
-### Node4:DM Cifar/Cinic-10
+### Node 1Purify Poisoned EBM
 (
-# train_classifier
-for i in 150 125 100 75
-do
-    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[cifar10_DDPM[250]_nf[L]]_T[$i]" --poison_type 'Narcissus';
-    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[cinic10_imagenet_DDPM[250]_nf[L]]_T[$i]" --poison_type 'Narcissus';
-    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[cifar10_DDPM[250]_nf[L]]_T[$i]" --poison_type 'Narcissus' --noise_eps_narcissus 16;
-    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[cinic10_imagenet_DDPM[250]_nf[L]]_T[$i]" --poison_type 'Narcissus' --noise_eps_narcissus 16;
-done
+python3 purify.py --remote_user 'sunaybhat' --diff_model None --ebm_name 'cifar10_nf[32]_NS[num=5000_size=32_eps=8]' --num_proc 8 --ebm_lang_steps 2000,1000,750,150;
+python3 purify.py --remote_user 'sunaybhat' --diff_model None --ebm_name 'cifar10_nf[32]_NS[num=5000_size=32_eps=8]' --ebm_lang_steps 150 --poison_type 'Narcissus';
+python3 purify.py --remote_user 'sunaybhat' --diff_model None --ebm_name 'cifar10_nf[32]_NS[num=5000_size=32_eps=8]' --num_proc 8 --ebm_lang_steps 2000,1000,750 --poison_type 'Narcissus' --noise_eps_narcissus 16;
 )
 
-### Node 7:DM Office-Home/Flowers-102
-(
-for i in 150 125 100 75
-do
-    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[office_home_DDPM[250]_nf[L]]_T[$i]" --poison_type 'Narcissus';
-    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[flowers102_DDPM[250]_nf[L]]_T[$i]" --poison_type 'Narcissus';
-    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[office_home_DDPM[250]_nf[L]]_T[$i]" --poison_type 'Narcissus' --noise_eps_narcissus 16;
-    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[flowers102_DDPM[250]_nf[L]]_T[$i]" --poison_type 'Narcissus' --noise_eps_narcissus 16;
-done
-)
+# Train classifer
+python3 train_classifier.py --remote_user 'sunaybhat' --data_key "EBM[cifar10_nf[32]_NS[num=5000_size=32_eps=8]]_Steps[150]_T[0.0001]" --poison_type 'Narcissus' --noise_eps_narcissus 16;
+python3 train_classifier.py --remote_user 'sunaybhat' --data_key "EBM[cifar10_nf[32]_NS[num=5000_size=32_eps=8]]_Steps[150]_T[0.0001]" --poison_type 'Narcissus';
 
-### Node 8:DM LFW/Textures
+
+
+
+
+### Node4: Purify DM
 (
-for i in 150 125 100 75
-do
-    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[lfw_people_DDPM[250]_nf[L]]_T[$i]" --poison_type 'Narcissus';
-    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[textures_DDPM[250]_nf[L]]_T[$i]" --poison_type 'Narcissus';
-    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[lfw_people_DDPM[250]_nf[L]]_T[$i]" --poison_type 'Narcissus' --noise_eps_narcissus 16;
-    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[textures_DDPM[250]_nf[L]]_T[$i]" --poison_type 'Narcissus' --noise_eps_narcissus 16;
-done
+python3 purify.py --remote_user 'sunaybhat' --ebm_model None --diff_name 'cifar10_DDPM[250]_nf[L]_NS[num=5000_size=32_eps=8]' --num_proc 8 --diff_T 150,125,100,75;
+python3 purify.py --remote_user 'sunaybhat' --ebm_model None --diff_name 'cifar10_DDPM[250]_nf[L]_NS[num=5000_size=32_eps=8]' --num_proc 8 --diff_T 150,125,100,75 --poison_type 'Narcissus';
+python3 purify.py --remote_user 'sunaybhat' --ebm_model None --diff_name 'cifar10_DDPM[250]_nf[L]_NS[num=5000_size=32_eps=8]' --num_proc 8 --diff_T 150,125,100,75 --poison_type 'Narcissus' --noise_eps_narcissus 16;
 )
 
 
@@ -67,10 +31,10 @@ done
 
 # Copy Models up to Node
 rsync -av --exclude='.DS_Store' /Users/sunaybhat/Documents/GitHub/data/PureGen_Models/* sunaybhat@node1:/home/sunaybhat/data/PureGen_Models/;
+rsync -av --exclude='.DS_Store' /Users/sunaybhat/Documents/GitHub/data/PureGen_Models/* sunaybhat@node2:/home/sunaybhat/data/PureGen_Models/;
 rsync -av --exclude='.DS_Store' /Users/sunaybhat/Documents/GitHub/data/PureGen_Models/* sunaybhat@node3:/home/sunaybhat/data/PureGen_Models/;
 rsync -av --exclude='.DS_Store' /Users/sunaybhat/Documents/GitHub/data/PureGen_Models/* sunaybhat@node4:/home/sunaybhat/data/PureGen_Models/;
 rsync -av --exclude='.DS_Store' /Users/sunaybhat/Documents/GitHub/data/PureGen_Models/* sunaybhat@node5:/home/sunaybhat/data/PureGen_Models/;
-rsync -av --exclude='.DS_Store' /Users/sunaybhat/Documents/GitHub/data/PureGen_Models/* sunaybhat@node6:/home/sunaybhat/data/PureGen_Models/;
 rsync -av --exclude='.DS_Store' /Users/sunaybhat/Documents/GitHub/data/PureGen_Models/* sunaybhat@node7:/home/sunaybhat/data/PureGen_Models/;
 rsync -av --exclude='.DS_Store' /Users/sunaybhat/Documents/GitHub/data/PureGen_Models/* sunaybhat@node8:/home/sunaybhat/data/PureGen_Models/;
 
