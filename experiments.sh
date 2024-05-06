86694714ce66b8a5be9ed16fb38de2d617bc8d4d
Sunay Bhat <56206141+SunayBhat1@users.noreply.github.com>
2024-05-06 12:41:45 -0700

    minor

--- experiments.sh
+++ experiments.sh
@@ -2,9 +2,37 @@
 # Nodes Lists #
 ###############
 
+### Node1: PoisonedGen Train EBM R18 HLB
+(
+for i in 150; do     
+    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "EBMSNGAN32[cifar10_NS[num=5000_size=32_eps=8]_nf[128]]_Steps[150]_T[0.0001]" --poison_type 'Narcissus' --config_overrides 'R18_HLB';    
+    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "EBMSNGAN32[cifar10_NS[num=5000_size=32_eps=16]_nf[128]]_Steps[150]_T[0.0001]" --poison_type 'Narcissus' --config_overrides 'R18_HLB';
+    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "EBMSNGAN32[cifar10_nf[128]]_Steps[150]_T[0.0001]" --poison_type 'Narcissus' --config_overrides 'R18_HLB';
+done;
+
+for i in 750 1000 2000; do     
+    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "EBMSNGAN32[cifar10_NS[num=5000_size=32_eps=8]_nf[128]]_Steps[$i]_T[0.0001]" --poison_type 'Narcissus' --config_overrides 'R18_HLB';     
+    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "EBMSNGAN32[cifar10_NS[num=5000_size=32_eps=16]_nf[128]]_Steps[$i]_T[0.0001]" --poison_type 'Narcissus' --config_overrides 'R18_HLB';
+    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "EBMSNGAN32[cifar10_nf[128]]_Steps[$i]_T[0.0001]" --poison_type 'Narcissus' --config_overrides 'R18_HLB';
+done; 
+)
+
+### Node4: PoisonedGen Train DM R18 HLB
+# (
+# python3 purify.py --remote_user 'sunaybhat' --ebm_model None --diff_name 'cifar10_DDPM[250]_nf[L]_NS[num=5000_size=32_eps=16]' --num_proc 8 --diff_T 150,125,100,75;
+# python3 purify.py --remote_user 'sunaybhat' --ebm_model None --diff_name 'cifar10_DDPM[250]_nf[L]_NS[num=5000_size=32_eps=16]' --num_proc 8 --diff_T 150,125,100,75 --poison_type 'Narcissus';
+# python3 purify.py --remote_user 'sunaybhat' --ebm_model None --diff_name 'cifar10_DDPM[250]_nf[L]_NS[num=5000_size=32_eps=16]' --num_proc 8 --diff_T 150,125,100,75 --poison_type 'Narcissus' --noise_eps_narcissus 16;
+
+# # Train classifer
+# for i in 150 125 100 75; do
+#     python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[cifar10_DDPM[250]_nf[L]_NS[num=5000_size=32_eps=16]]_T[$i]" --poison_type 'Narcissus';
+#     python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[cifar10_DDPM[250]_nf[L]_NS[num=5000_size=32_eps=16]]_T[$i]" --poison_type 'Narcissus' --noise_eps_narcissus 16;
+# done
+# )
+
 ### Node 3 NTG Res18 HLB Trainig
 (
-    for i in 75; do # 100 125 150
+    for i in 100 125 150; do # 100 125 150
         python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[cifar10_DDPM[250]_nf[L]]_T[$i]" --poison_type 'NeuralTangent' --config_overrides 'R18_HLB';
         python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[cinic10_imagenet_DDPM[250]_nf[L]]_T[$i]" --poison_type 'NeuralTangent' --config_overrides 'R18_HLB';
         python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[office_home_DDPM[250]_nf[L]]_T[$i]" --poison_type 'NeuralTangent' --config_overrides 'R18_HLB';
@@ -16,7 +44,7 @@
         python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[textures_DDPM[250]_nf[L]]_T[$i]" --poison_type 'NeuralTangent' --config_overrides 'R18_HLB';
     done
 
-    for i in 750; do # 700 750 800 850 
+    for i in 700 800 850 ; do # 700 750 800 850 
         python3 train_classifier.py --remote_user 'sunaybhat' --data_key "EBMSNGAN32[cifar10_nf[128]]_Steps[$i]_T[0.0001]" --poison_type 'NeuralTangent' --config_overrides 'R18_HLB';
         python3 train_classifier.py --remote_user 'sunaybhat' --data_key "EBMSNGAN32[cinic10_imagenet_nf[128]]_Steps[$i]_T[0.0001]" --poison_type 'NeuralTangent' --config_overrides 'R18_HLB';
         python3 train_classifier.py --remote_user 'sunaybhat' --data_key "EBMSNGAN32[office_home_nf[128]]_Steps[$i]_T[0.0001]" --poison_type 'NeuralTangent' --config_overrides 'R18_HLB';
@@ -28,6 +56,78 @@
         python3 train_classifier.py --remote_user 'sunaybhat' --data_key "EBMSNGAN32[textures_nf[128]]_Steps[$i]_T[0.0001]" --poison_type 'NeuralTangent' --config_overrides 'R18_HLB';
     done
 )
+
+
+### Node2: Purify EBM POOD
+(
+python3 purify.py --remote_user 'sunaybhat' --diff_model None --num_proc 8   \
+    --ebm_name 'cifar10_nf[128]','cifar10_nf[128]','cifar10_nf[128]','cifar10_nf[128]','cinic10_imagenet_nf[128]','cinic10_imagenet_nf[128]','cinic10_imagenet_nf[128]','cinic10_imagenet_nf[128]' \
+    --ebm_lang_steps 2000,1000,750,150,2000,1000,750,150;
+python3 purify.py --remote_user 'sunaybhat' --diff_model None --num_proc 8  \
+    --ebm_name 'flowers102_nf[128]','flowers102_nf[128]','flowers102_nf[128]','flowers102_nf[128]','fgvc_aircraft_nf[128]','fgvc_aircraft_nf[128]','fgvc_aircraft_nf[128]','fgvc_aircraft_nf[128]' \
+    --ebm_lang_steps 2000,1000,750,150,2000,1000,750,150;
+python3 purify.py --remote_user 'sunaybhat' --diff_model None --num_proc 8  \
+    --ebm_name 'food101_nf[128]','food101_nf[128]','food101_nf[128]','food101_nf[128]','lfw_people_nf[128]','lfw_people_nf[128]','lfw_people_nf[128]','lfw_people_nf[128]' \
+    --ebm_lang_steps 2000,1000,750,150,2000,1000,750,150;
+python3 purify.py --remote_user 'sunaybhat' --diff_model None --num_proc 8  \
+    --ebm_name 'office_home_nf[128]','office_home_nf[128]','office_home_nf[128]','office_home_nf[128]','oxford_iiit_pet_nf[128]','oxford_iiit_pet_nf[128]','oxford_iiit_pet_nf[128]','oxford_iiit_pet_nf[128]' \
+    --ebm_lang_steps 2000,1000,750,150,2000,1000,750,150;
+python3 purify.py --remote_user 'sunaybhat' --diff_model None --num_proc 8  \
+    --ebm_name 'textures_nf[128]' \
+    --ebm_lang_steps 2000,1000,750,150;
+)
+
+
+
+  
+### Node4: Purify POOD DM (cifar10,cinic10_imagenet)
+(
+#train classifier
+for i in 150 125 100 75; do
+    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[cifar10_DDPM[250]_nf[L]]_T[$i]" --poison_type 'Narcissus' --config_overrides 'R18_HLB';
+    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[cifar10_DDPM[250]_nf[L]]_T[$i]" --poison_type 'Narcissus' --noise_eps_narcissus 16 --config_overrides 'R18_HLB';
+    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[cinic10_imagenet_DDPM[250]_nf[L]]_T[$i]" --poison_type 'Narcissus' --config_overrides 'R18_HLB';
+    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[cinic10_imagenet_DDPM[250]_nf[L]]_T[$i]" --poison_type 'Narcissus' --noise_eps_narcissus 16 --config_overrides 'R18_HLB';
+done
+)
+
+### Node5: Purify POOD DM (oxford, food, aircraft)
+(
+# Train classifer R18 HLB
+for i in 150 125 100 75; do
+    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[oxford_iiit_pet_DDPM[250]_nf[L]]_T[$i]" --poison_type 'Narcissus' --config_overrides 'R18_HLB';
+    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[oxford_iiit_pet_DDPM[250]_nf[L]]_T[$i]" --poison_type 'Narcissus' --noise_eps_narcissus 16 --config_overrides 'R18_HLB';
+    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[food101_DDPM[250]_nf[L]]_T[$i]" --poison_type 'Narcissus' --config_overrides 'R18_HLB';
+    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[food101_DDPM[250]_nf[L]]_T[$i]" --poison_type 'Narcissus' --noise_eps_narcissus 16 --config_overrides 'R18_HLB';
+    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[fgvc_aircraft_DDPM[250]_nf[L]]_T[$i]" --poison_type 'Narcissus' --config_overrides 'R18_HLB';
+    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[fgvc_aircraft_DDPM[250]_nf[L]]_T[$i]" --poison_type 'Narcissus' --noise_eps_narcissus 16 --config_overrides 'R18_HLB';
+done
+)
+
+### Node7: Purify POOD DM (flowers, office-home)
+(
+#train classifier
+for i in 150 125 100 75; do
+    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[flowers102_DDPM[250]_nf[L]]_T[$i]" --poison_type 'Narcissus' --config_overrides 'R18_HLB';
+    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[flowers102_DDPM[250]_nf[L]]_T[$i]" --poison_type 'Narcissus' --noise_eps_narcissus 16 --config_overrides 'R18_HLB';
+    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[office_home_DDPM[250]_nf[L]]_T[$i]" --poison_type 'Narcissus' --config_overrides 'R18_HLB';
+    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[office_home_DDPM[250]_nf[L]]_T[$i]" --poison_type 'Narcissus' --noise_eps_narcissus 16 --config_overrides 'R18_HLB';
+done
+)
+
+### Node8: Purify POOD DM (lfw_people, textures)
+(
+#train classifier
+for i in 150 125 100 75; do
+    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[lfw_people_DDPM[250]_nf[L]]_T[$i]" --poison_type 'Narcissus' --config_overrides 'R18_HLB';
+    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[lfw_people_DDPM[250]_nf[L]]_T[$i]" --poison_type 'Narcissus' --noise_eps_narcissus 16 --config_overrides 'R18_HLB';
+    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[textures_DDPM[250]_nf[L]]_T[$i]" --poison_type 'Narcissus' --config_overrides 'R18_HLB';
+    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[textures_DDPM[250]_nf[L]]_T[$i]" --poison_type 'Narcissus' --noise_eps_narcissus 16 --config_overrides 'R18_HLB';
+done
+)
+
+
+
 
 ### Node 1 Purify Poisoned EBM
 (
@@ -63,63 +163,14 @@ done
 )
 
 
-### Node2: Purify EBM POOD
-(
-python3 purify.py --remote_user 'sunaybhat' --diff_model None --num_proc 8   \
-    --ebm_name 'cifar10_nf[128]','cifar10_nf[128]','cifar10_nf[128]','cifar10_nf[128]','cinic10_imagenet_nf[128]','cinic10_imagenet_nf[128]','cinic10_imagenet_nf[128]','cinic10_imagenet_nf[128]' \
-    --ebm_lang_steps 2000,1000,750,150,2000,1000,750,150;
-(
-python3 purify.py --remote_user 'sunaybhat' --diff_model None --num_proc 8  \
-    --ebm_name 'flowers102_nf[128]','flowers102_nf[128]','flowers102_nf[128]','flowers102_nf[128]','fgvc_aircraft_nf[128]','fgvc_aircraft_nf[128]','fgvc_aircraft_nf[128]','fgvc_aircraft_nf[128]' \
-    --ebm_lang_steps 2000,1000,750,150,2000,1000,750,150;
-python3 purify.py --remote_user 'sunaybhat' --diff_model None --num_proc 8  \
-    --ebm_name 'food101_nf[128]','food101_nf[128]','food101_nf[128]','food101_nf[128]','lfw_people_nf[128]','lfw_people_nf[128]','lfw_people_nf[128]','lfw_people_nf[128]' \
-    --ebm_lang_steps 2000,1000,750,150,2000,1000,750,150;
-python3 purify.py --remote_user 'sunaybhat' --diff_model None --num_proc 8  \
-    --ebm_name 'office_home_nf[128]','office_home_nf[128]','office_home_nf[128]','office_home_nf[128]','oxford_iiit_pet_nf[128]','oxford_iiit_pet_nf[128]','oxford_iiit_pet_nf[128]','oxford_iiit_pet_nf[128]' \
-    --ebm_lang_steps 2000,1000,750,150,2000,1000,750,150;
-python3 purify.py --remote_user 'sunaybhat' --diff_model None --num_proc 8  \
-    --ebm_name 'textures_nf[128]' \
-    --ebm_lang_steps 2000,1000,750,150;
-)
-
-
-### Node4: Purify DM
-(
-python3 purify.py --remote_user 'sunaybhat' --ebm_model None --diff_name 'cifar10_DDPM[250]_nf[L]_NS[num=5000_size=32_eps=16]' --num_proc 8 --diff_T 150,125,100,75;
-python3 purify.py --remote_user 'sunaybhat' --ebm_model None --diff_name 'cifar10_DDPM[250]_nf[L]_NS[num=5000_size=32_eps=16]' --num_proc 8 --diff_T 150,125,100,75 --poison_type 'Narcissus';
-python3 purify.py --remote_user 'sunaybhat' --ebm_model None --diff_name 'cifar10_DDPM[250]_nf[L]_NS[num=5000_size=32_eps=16]' --num_proc 8 --diff_T 150,125,100,75 --poison_type 'Narcissus' --noise_eps_narcissus 16;
-
-# Train classifer
-for i in 150 125 100 75; do
-    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[cifar10_DDPM[250]_nf[L]_NS[num=5000_size=32_eps=16]]_T[$i]" --poison_type 'Narcissus';
-    python3 train_classifier.py --remote_user 'sunaybhat' --data_key "DM_UNET[cifar10_DDPM[250]_nf[L]_NS[num=5000_size=32_eps=16]]_T[$i]" --poison_type 'Narcissus' --noise_eps_narcissus 16;
-done
-)
-  
-
-
-### Node5: Purify POOD DM
-(
-python3 purify.py --remote_user 'sunaybhat' --ebm_model None --diff_name 'oxford_iiit_pet_DDPM[250]_nf[L]' --num_proc 8 --diff_T 150,125,100,75;
-python3 purify.py --remote_user 'sunaybhat' --ebm_model None --diff_name 'oxford_iiit_pet_DDPM[250]_nf[L]' --num_proc 8 --diff_T 150,125,100,75 --poison_type 'Narcissus';
-python3 purify.py --remote_user 'sunaybhat' --ebm_model None --diff_name 'oxford_iiit_pet_DDPM[250]_nf[L]' --num_proc 8 --diff_T 150,125,100,75 --poison_type 'Narcissus' --noise_eps_narcissus 16;
-
-python3 purify.py --remote_user 'sunaybhat' --ebm_model None --diff_name 'food101_DDPM[250]_nf[L]' --num_proc 8 --diff_T 150,125,100,75;
-python3 purify.py --remote_user 'sunaybhat' --ebm_model None --diff_name 'food101_DDPM[250]_nf[L]' --num_proc 8 --diff_T 150,125,100,75 --poison_type 'Narcissus';
-python3 purify.py --remote_user 'sunaybhat' --ebm_model None --diff_name 'food101_DDPM[250]_nf[L]' --num_proc 8 --diff_T 150,125,100,75 --poison_type 'Narcissus' --noise_eps_narcissus 16;
-
-python3 purify.py --remote_user 'sunaybhat' --ebm_model None --diff_name 'fgvc_aircraft_DDPM[250]_nf[L]' --num_proc 8 --diff_T 150,125,100,75;
-python3 purify.py --remote_user 'sunaybhat' --ebm_model None --diff_name 'fgvc_aircraft_DDPM[250]_nf[L]' --num_proc 8 --diff_T 150,125,100,75 --poison_type 'Narcissus';
-python3 purify.py --remote_user 'sunaybhat' --ebm_model None --diff_name 'fgvc_aircraft_DDPM[250]_nf[L]' --num_proc 8 --diff_T 150,125,100,75 --poison_type 'Narcissus' --noise_eps_narcissus 16;
-)
-
-
-
 
 ############################
 # Setup Node and Copy Data #
 ############################
+
+rsync -av sunaybhat@node1:/home/sunaybhat/data/PureGen_PoisonDefense/* /Users/sunaybhat/Documents/GitHub/data/PureGen_PoisonDefense/;
+rsync -av --exclude='.DS_Store' sunaybhat@node1:/home/sunaybhat/data/PureGen_Models/ /Users/sunaybhat/Documents/GitHub/data/PureGen_PoisonDefense/;
+
 
 # Copy Models up to Node
 rsync -av --exclude='.DS_Store' /Users/sunaybhat/Documents/GitHub/data/PureGen_Models/* sunaybhat@node1:/home/sunaybhat/data/PureGen_Models/;
